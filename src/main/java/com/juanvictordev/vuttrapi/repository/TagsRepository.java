3327d31ffc5d9c609eefa4df9e79c7bcde9390b7
juanvictorDev <juanvictor.dev@gmail.com>
2024-05-12 15:00:12 -0300

    ajuste no nome da classe TagsRepository

--- src/main/java/com/juanvictordev/vuttrapi/repository/TagsRepository.java
+++ src/main/java/com/juanvictordev/vuttrapi/repository/TagsRepository.java
@@ -3,6 +3,6 @@ package com.juanvictordev.vuttrapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.juanvictordev.vuttrapi.entity.Tags;
 
-public interface TagsReposiroty extends JpaRepository<Tags, String>{
+public interface TagsRepository extends JpaRepository<Tags, String>{
   
 }
