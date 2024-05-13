f2833cea6a7aa18be8daf878678411c79fabd5f7
juanvictorDev <juanvictor.dev@gmail.com>
2024-05-13 18:06:48 -0300

    implementado MethodArgumentTypeMismatchException handler no ControllerAdvice

--- src/main/java/com/juanvictordev/vuttrapi/error/GlobalExceptionHandler.java
+++ src/main/java/com/juanvictordev/vuttrapi/error/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.juanvictordev.vuttrapi.error;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
 import com.juanvictordev.vuttrapi.dto.ErrorBeanValidationDto;
 
 
@@ -14,7 +17,7 @@ import com.juanvictordev.vuttrapi.dto.ErrorBeanValidationDto;
 public class GlobalExceptionHandler {
   
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+  public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex){
     
     //CRIANDO UMA NOVA RESPOSTA PERSONALIZADA PARA ERRO NO BEAN VALIDATION 
 
@@ -28,5 +31,15 @@ public class GlobalExceptionHandler {
     });
 
     return ResponseEntity.badRequest().body(Map.of("errors", fields));
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<Map<String, Object>> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex){
+    Map<String, Object> erro = new HashMap<>();
+    erro.put("error", 400);
+    erro.put("message", ex.getMessage());
+    erro.put("local", ex.getPropertyName());
+
+    return ResponseEntity.badRequest().body(erro);
   }
 }
