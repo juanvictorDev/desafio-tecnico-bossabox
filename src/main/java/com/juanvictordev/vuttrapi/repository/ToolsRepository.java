ea729b86df9075df09a765088b31bff9e4e81577
juanvictorDev <juanvictor.dev@gmail.com>
2024-05-13 17:48:27 -0300

    correcao do bug nos atributos da pagination pela url

--- src/main/java/com/juanvictordev/vuttrapi/repository/ToolsRepository.java
+++ src/main/java/com/juanvictordev/vuttrapi/repository/ToolsRepository.java
@@ -9,13 +9,16 @@ import com.juanvictordev.vuttrapi.entity.Tools;
 
 public interface ToolsRepository extends JpaRepository<Tools, Integer>{
   
-  // METODO COM QUERY PERSONALIZADA, PARA TRAZER TODAS AS TOOLS QUE
-  // POSSUEM UMA DETERMINADA TAG 
-  @Query(value = 
-    "SELECT tools.* " +
-    "FROM tools INNER JOIN tool_tag " +
-    "ON tools.tool_id = tool_tag.tool_id " +
-    "WHERE tool_tag.tag_name = :tag",
+  // METODO COM QUERY PERSONALIZADA, PARA TRAZER TODAS AS TOOLS QUE POSSUEM UMA DETERMINADA TAG 
+  // QUANDO SE USA SQL NATIVO PARA PAGINATION, PRECISA COLOCAR O PARAMETRO NATIVEQUERY
+  // COM A CONTAGEM DE DADOS PARA EVITAR BUG AO PASSAR VALORES COMO PARAMETRO NA URL
+  @Query(
+    value = 
+      "SELECT tools.* FROM tools INNER JOIN tool_tag " +
+      "ON tools.tool_id = tool_tag.tool_id WHERE tool_tag.tag_name = :tag",
+    countQuery = 
+      "SELECT count(*) FROM tools INNER JOIN tool_tag " + 
+      "ON tools.tool_id = tool_tag.tool_id WHERE tool_tag.tag_name = :tag",
     nativeQuery = true
   )
   Page<Tools> findByTagName(@Param("tag") String tag, Pageable pageable);
